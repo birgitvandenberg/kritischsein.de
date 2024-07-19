f5fab2932648d2c0504377f5931edb06dcc8d3a3
birgitvandenberg <133874087+birgitvandenberg@users.noreply.github.com>
2024-07-19 02:06:19 +0200

    automated deployment 2024-07-19 02:06:19

---
+++ docs/wp-content/themes/twentytwelve/content-none.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<?xml encoding="UTF-8"><html><body><article id="post-0" class="post no-results not-found">
+		<header class="entry-header">
+			<h1 class="entry-title"></h1></header></article></body></html>
+<!-- Powered by Staatic (https://staatic.com/) -->
